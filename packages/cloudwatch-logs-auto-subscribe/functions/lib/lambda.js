e41560782c463162c927eb70f12f2db0f8428c4e
Stan Miroshnikov <mirstan@gmail.com>
2021-06-04 19:51:05 -0400

    fix uuidv4 import

--- packages/cloudwatch-logs-auto-subscribe/functions/lib/lambda.js
+++ packages/cloudwatch-logs-auto-subscribe/functions/lib/lambda.js
@@ -1,9 +1,6 @@
 const AWS = require("./aws");
 const lambda = new AWS.Lambda();
-const { 
-	v1: uuidv1,
-	v4: uuidv4,
-  } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const addLambdaPermission = async (functionArn) => {
 	const req = {
